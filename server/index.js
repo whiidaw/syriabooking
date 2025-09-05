62c8eda302d057c275f3738c770b89588af2fbd0
nourdrmosh <nour.drmosh@gmail.com>
2025-09-05 21:19:34 +0300

    Add database name to MongoDB connection string

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO + 'syriabooking');
     console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
